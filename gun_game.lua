c71a3f76100ba788347a33880b4c39eda1adda02
Sean Tucker <R00Ki3@users.noreply.github.com>
2019-01-19 22:58:52 -0600

    Inital commit

---
+++ gun_game.lua
@@ -0,0 +1,103 @@
+-- List of Weapons that will be given to players in order.
+local weapon_list = {
+	'ff_weapon_rpg',
+	'ff_weapon_crowbar',
+	'ff_weapon_shotgun',
+}
+
+local player_table = {}
+local functionList = {
+	startup = startup,
+	player_spawn = player_spawn,
+	player_onchat = player_onchat,
+	player_killed = player_killed
+ }
+
+-- Game Callbacks
+
+function startup()
+	if type(functionList.startup) == "function" then
+		functionList.startup()
+	end
+end
+
+function player_connected(player)
+	local player = CastToPlayer(player)
+	
+	player_table[player:GetSteamID()] = {
+		current_weapon = 1
+	}
+end
+
+function player_spawn(player)
+	if type(functionList.player_spawn) == "function" then
+		functionList.player_spawn(player)
+	end
+
+	local player = CastToPlayer(player)
+	player:RemoveAllWeapons()
+	player:GiveWeapon(GetCurrentWeapon(player), true)
+	RestockFull(player)
+end
+
+function player_killed(player, damageinfo)
+	if type(functionList.player_killed) == "function" then
+		functionList.player_killed(player, damageinfo)
+	end
+
+	local player = CastToPlayer(player)
+	if not damageinfo or then return end
+    local attacker = CastToPlayer(damageinfo:GetAttacker())
+	if not IsPlayer(attacker) then return end
+
+	if not (player:GetTeamId() == attacker:GetTeamId()) then
+		GiveNextWeapon(attacker)
+	end
+end
+
+-- Do stuff when a player chats.
+function player_onchat(player, chatstring)	
+	local player = CastToPlayer(player)
+	local message = string.sub( string.gsub( chatstring, "%c", "" ), string.len(player:GetName())+3 )
+	
+	if message == "!next" then
+		GiveNextWeapon(player)
+		return false
+	end
+
+	return true -- All other chat should be allowed.
+end
+
+-- Utility functions
+
+-- Give the next weapon if the player doesn't win.
+function GiveNextWeapon(player)
+	player_table[player:GetSteamID()].current_weapon = player_table[player:GetSteamID()].current_weapon + 1
+	
+	if not CheckForWin(player) then
+		player:RemoveAllWeapons()
+		player:GiveWeapon(GetCurrentWeapon(player), true)
+		RestockFull(player)
+	end
+end
+
+-- Resupply a player with full of everything
+function RestockFull(player)
+	player:AddAmmo( Ammo.kNails, 400 )
+	player:AddAmmo( Ammo.kShells, 400 )
+	player:AddAmmo( Ammo.kRockets, 400 )
+	player:AddAmmo( Ammo.kCells, 400 )
+end
+-- get the current weapon a player has
+function GetCurrentWeapon(player)
+	return weapon_list[player_table[player:GetSteamID()].current_weapon]
+end
+
+-- If a player wins then end the game.
+function CheckForWin(player)
+	if player_table[player:GetSteamID()].current_weapon > table.getn(weapon_list) then
+		ChatToAll(player:GetName().." Wins!")
+		GoToIntermission()
+		return true
+	end	
+end
